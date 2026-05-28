4745157ed43df846195eb2b9f8c2340a8cef7bb6
Karla <karla456p32@gmail.com>
2026-05-28 21:43:14 +0000

    Updated RecyclingGuidanceServiceTest class after full implementation of guidance messages for category constant values that the material type is allowed to have.

--- app/src/test/java/com/mightyfour/RecyclingGuidanceServiceTest.java
+++ app/src/test/java/com/mightyfour/RecyclingGuidanceServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 
 import com.mightyfour.domain.Type;
 import com.mightyfour.application.RecyclingGuidanceService;
+import com.mightyfour.DefaultRecyclingGuidanceFactory;
 import com.mightyfour.domain.Material;
 import com.mightyfour.domain.RecyclingGuidanceFactory;
 
@@ -13,19 +14,19 @@ public class RecyclingGuidanceServiceTest {
         @Test
         public void shouldCorreclyReturnGuidanceMessageForPlastic(){
           //ARRANGE
-          Material material = new Material("PVC Plastic", "Dispose of plastic waste in the bin 3 with the 'plastic packaging' label.", Type.PLASTIC , 2.1);
+          Material material = new Material("PET Plastic", "Dispose of plastic waste in the bin with the 'plastic packaging' label.", Type.PLASTIC, 3.5);
           ArrayList<Material> materials = new ArrayList<>();
           materials.add(material);
 
 
           //ACT
-          RecyclingGuidanceService service = new RecyclingGuidanceService(new RecyclingGuidanceFactory());
+          RecyclingGuidanceService service = new RecyclingGuidanceService(new DefaultRecyclingGuidanceFactory());
           ArrayList<String> returnedMessages = service.retrieveMessages(materials);
 
 
           //ASSERT
           ArrayList<String> expectedMessages = new ArrayList<>();
-          expectedMessages.add("Plastic ---> Put in grey trashbin");
+          expectedMessages.add("Plastic ---> Put in orange trashbin.");
         
           assertEquals(expectedMessages, returnedMessages);
         }
@@ -34,13 +35,13 @@ public class RecyclingGuidanceServiceTest {
         @Test
         public void shouldCorreclyReturnGuidanceMessageForPaper(){
           //ARRANGE
-          Material material = new Material("Paper", "Dispose of plastic waste in the bin 2 with the 'paper packaging' label.", Type.PAPER , 2.1);
+          Material material = new Material("Recycled Paperboard", "Dispose of paper waste in the bin with the 'paper packaging' label.", Type.PAPER , 0.50); 
           ArrayList<Material> materials = new ArrayList<>();
           materials.add(material);
 
 
           //ACT
-          RecyclingGuidanceService service = new RecyclingGuidanceService(new RecyclingGuidanceFactory());
+          RecyclingGuidanceService service = new RecyclingGuidanceService(new DefaultRecyclingGuidanceFactory());
           ArrayList<String> returnedMessages = service.retrieveMessages(materials);
 
 
@@ -51,26 +52,101 @@ public class RecyclingGuidanceServiceTest {
           assertEquals(expectedMessages, returnedMessages);
         }
 
+        public void shouldCorreclyReturnGuidanceMessageForMetal(){
+          //ARRANGE
+          Material material = new Material("Virgin Aluminum", "Dispose of metal waste in the bin with the 'metal packaging' label.", Type.METAL , 12.0);;
+          ArrayList<Material> materials = new ArrayList<>();
+          materials.add(material);
+
+
+          //ACT
+          RecyclingGuidanceService service = new RecyclingGuidanceService(new DefaultRecyclingGuidanceFactory());
+          ArrayList<String> returnedMessages = service.retrieveMessages(materials);
+
+
+          //ASSERT
+          ArrayList<String> expectedMessages = new ArrayList<>();
+          expectedMessages.add("Metal ---> Put in grey trashbin.");
+        
+          assertEquals(expectedMessages, returnedMessages);
+        }
+
+        public void shouldCorreclyReturnGuidanceMessageForGlass(){
+          //ARRANGE
+          Material material = new Material("Virgin Glass", "Dispose of glass waste in the bin with the 'glass packaging' label.", Type.GLASS , 1.1);
+          ArrayList<Material> materials = new ArrayList<>();
+          materials.add(material);
+
+
+          //ACT
+          RecyclingGuidanceService service = new RecyclingGuidanceService(new DefaultRecyclingGuidanceFactory());
+          ArrayList<String> returnedMessages = service.retrieveMessages(materials);
+
+
+          //ASSERT
+          ArrayList<String> expectedMessages = new ArrayList<>();
+          expectedMessages.add("Glass ---> Put in teal trashbin.");
+        
+          assertEquals(expectedMessages, returnedMessages);
+        }
+
+        public void shouldCorreclyReturnGuidanceMessageForOrganic(){
+          //ARRANGE
+          Material material = new Material("Natural Rubber", "Dispose of organic waste in the bin with the 'organic waste' label.", Type.ORGANIC , 1.3);
+          ArrayList<Material> materials = new ArrayList<>();
+          materials.add(material);
+
+
+          //ACT
+          RecyclingGuidanceService service = new RecyclingGuidanceService(new DefaultRecyclingGuidanceFactory());
+          ArrayList<String> returnedMessages = service.retrieveMessages(materials);
+
+
+          //ASSERT
+          ArrayList<String> expectedMessages = new ArrayList<>();
+          expectedMessages.add("Organic ---> Put in green/dark green trashbin.");
+        
+          assertEquals(expectedMessages, returnedMessages);
+        }
+
+        public void shouldCorreclyReturnGuidanceMessageForTextiles(){
+          //ARRANGE
+          Material material = new Material("Fleece", "Dispose of synthetic textiles in the bin with the 'synthetic textile' label.", Type.SYNTETIC_TEXTILE, 8.7);
+          ArrayList<Material> materials = new ArrayList<>();
+          materials.add(material);
+
+
+          //ACT
+          RecyclingGuidanceService service = new RecyclingGuidanceService(new DefaultRecyclingGuidanceFactory());
+          ArrayList<String> returnedMessages = service.retrieveMessages(materials);
+
+
+          //ASSERT
+          ArrayList<String> expectedMessages = new ArrayList<>();
+          expectedMessages.add("Non-organic textiles --> Put in light green trashbin.");
+        
+          assertEquals(expectedMessages, returnedMessages);
+        }
 
         @Test
         public void shouldCorreclyReturnGuidanceMessageForMixed(){
           //ARRANGE
-          Material paperA = new Material("Paper a", "Dispose of paper waste in the bin 2 with the 'paper packaging' label.", Type.PAPER , 0.4);
-          Material plasticB = new Material("Plastic b", "Dispose of paper waste in the bin 3 with the 'plastic packaging' label.", Type.PLASTIC , 0.5);
+          Material paperA = new Material("Recycled Paperboard", "Dispose of paper waste in the bin with the 'paper packaging' label.", Type.PAPER , 0.50);
+          Material plasticB = new Material("PET Plastic", "Dispose of plastic waste in the bin with the 'plastic packaging' label.", Type.PLASTIC, 3.5);
           ArrayList<Material> materials = new ArrayList<>();
           materials.add(paperA);
           materials.add(plasticB);
 
 
           //ACT
-          RecyclingGuidanceService service = new RecyclingGuidanceService(new RecyclingGuidanceFactory());
+          RecyclingGuidanceService service = new RecyclingGuidanceService(new DefaultRecyclingGuidanceFactory());
           ArrayList<String> returnedMessages = service.retrieveMessages(materials);
 
 
           //ASSERT
           ArrayList<String> expectedMessages = new ArrayList<>();
-          expectedMessages.add("Plastic ---> Put in grey trashbin");
           expectedMessages.add("Paper ---> Put in blue trashbin.");
+          expectedMessages.add("Plastic ---> Put in orange trashbin.");
         
           assertEquals(expectedMessages, returnedMessages);
         }
