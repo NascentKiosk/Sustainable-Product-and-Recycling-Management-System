1ebfbd91b0fb760d984792174092d409c735dd1a
Karla <karla456p32@gmail.com>
2026-05-30 17:29:48 +0000

    Re-added calculation logic to weighted sum strategy for environmental impact.

--- app/src/main/java/com/mightyfour/application/WeightedSumStrategy.java
+++ app/src/main/java/com/mightyfour/application/WeightedSumStrategy.java
@@ -2,10 +2,22 @@ package com.mightyfour.application;
 
 import com.mightyfour.domain.ImpactCalculationStrategy;
 import com.mightyfour.domain.Product;
+import com.mightyfour.domain.Material;
 import java.util.ArrayList;
 
 public class WeightedSumStrategy implements ImpactCalculationStrategy{
     public double calculateImpact(Product product, ArrayList<Double> materialWeights){
-        return 0.0; //Not implemented yet.
+        ArrayList<Material> materialsList = product.getMaterialsList();
+
+
+        double rawProductFootprint = 0;
+        int count = 0;
+        for(Material material : materialsList){
+            rawProductFootprint += material.getImpact() * materialWeights.get(count);
+            count++;
+        }
+
+
+        return rawProductFootprint;
     }
 }
